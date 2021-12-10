574093d2688a9203239661f8323b38cf296bad00
Omayma Majid <58831031+OmaymaMajid@users.noreply.github.com>
2021-12-10 22:51:06 +0100

    commit

---
+++ microservice-produits/src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (0, 'Bougie fonctionnant au feu', 'bougie qui fonctionne comme une ampoule mais sans électricité !', 'https://cdn.futura-sciences.com/buildsv6/images/mediumoriginal/f/f/f/fff574293a_27285_10317-bougie02.jpg', 22.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (1, 'Chaise pour s''assoire', 'Chaise rare avec non pas 1 ni 2 mais 3 pieds', 'https://www.ikea.com/fr/fr/images/products/janolof-chaise-de-salle-a-manger-pivotante-gunnared-gris-fonce__0972071_pe811513_s5.jpg?f=s', 95.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (2, 'Cheval pour nains', 'Ce cheval ne portera certainement pas blanche neige, mais sans problème les nains', 'https://www.aunainbleu.com/221-home_default/cheval-a-bascule.jpg', 360.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (3, 'Coq of steel, le superman des volailles', 'Ne passe pas au four', 'https://files.gobel.be/cache/products/images/cache_600x600/11498498_00608785f80ad77d1bf430052055d1cdbae2ee05.jpg', 620.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (4, 'Flacon à frotter avec un génie dedans', 'Vous donne droit à l''équivalent de 3/0 voeux', 'https://thumbs.dreamstime.com/b/lampe-magique-de-frottement-avec-les-mains-femelles-d-isolement-sur-le-fond-blanc-concept-pour-souhait-la-chance-et-magie-125814558.jpg', 1200.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (5, 'Horloge quantique', 'Donne l''heure, les minutes et même les secondes. Ne fait pas de café', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCYAhUSxKZdcFPW0eAVFrWDzMHNlYxgyvSDg&usqp=CAU', 180.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (6, 'Table d''opération pour Hamsters', 'Pour réaliser vos opérations chirugicales sur votre Hamster!', 'https://www.ikea.com/ma/en/images/products/ekedalen-extendable-table-oak__0736964_pe740828_s5.jpg?f=s', 210.0);
+
+INSERT INTO PRODUCT (ID , TITRE ,DESCRIPTION ,IMAGE ,PRIX ) VALUES (7 , 'Vase ayant appartenu a Zeus', 'Risque de choc électrique', 'https://m.media-amazon.com/images/I/6158rlWa8ML._AC_SX466_.jpg', 730.0);
